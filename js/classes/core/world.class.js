eaee7175fe4e4cbe5f5b9e4adfde7a25759c63b0
Michael Ring <michael.ring.87@web.de>
2025-11-03 16:25:03 +0100

    fixed multi throw bottles

--- js/classes/core/world.class.js
+++ js/classes/core/world.class.js
@@ -52,9 +52,10 @@ class World {
      * and bottles are available. Updates status bar.
      */
     checkThrowObjects() {
-        if(this.keyboard.THROW && (this.bottleCount > 0)) {
+        if(this.keyboard.THROW && (this.bottleCount > 0) && this.throwableObjects.length < 1) {
             let bottle = new ThrowableObject(this.character.x, this.character.y, this.bottleHitsEnemy);
             this.throwableObjects.push(bottle);
+            console.log(this.throwableObjects);
             this.bottleCount --
             this.changeStatusBar(this.bottleCount , this.level.maxBottles , "statusBarBottle")
         }
